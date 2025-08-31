1c9e2f64d67a26972392e7e9eaed73c43c7250ee
truongdang <quangtruonglf@gmail.com>
2025-08-31 14:37:14 +0700

    gazebo runnning with command received from controller

--- swervebot_bringup/launch/swervebot_sim.launch.py
+++ swervebot_bringup/launch/swervebot_sim.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
